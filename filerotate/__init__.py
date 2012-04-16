cf6abd13c063b2c72a5ec84035702da051a74c4b
Alen Mujezinovic <flashingpumpkin@gmail.com>
2012-04-16 14:43:06 +0100

    Added backwards rotation

--- filerotate/__init__.py
+++ filerotate/__init__.py
@@ -3,10 +3,7 @@ import __version__
 
 __version__ = __version__.__version__
 
-
-def rotate():
-    from __main__ import files
-
+def forward(files):
     files.sort()
     files.reverse()
 
@@ -15,7 +12,30 @@ def rotate():
             name, ext = os.path.splitext(file)
             os.rename(file, "{0}.{1}".format(name, int(ext[1:]) + 1))
         except ValueError: # Not a number extension
-            os.rename(file, "{0}.{1}".format(file, 0))
+            os.rename(file, "{0}.{1}".format(file, 0))    
+            
+def backward(files):
+    files.sort()
+
+    for file in files:
+        try:
+            name, ext = os.path.splitext(file)
+            ext = int(ext[1:])
+            if ext == 0:
+                os.rename(file, name)
+            else:
+                os.rename(file, "{0}.{1}".format(name, ext - 1))
+        except ValueError: # Not a number extension, nothing to rotate
+            pass
+
+def rotate():
+    from __main__ import opts, files
+
+    if opts.back:
+        backward(files)
+    else:
+        forward(files)
+    
 
 if __name__ == "__main__":
     rotate()
--- filerotate/__main__.py
+++ filerotate/__main__.py
@@ -3,9 +3,11 @@ import optparse
 import sys
 
 parser = optparse.OptionParser()
-parser.usage = '%prog [glob]'
+parser.usage = '%prog [pattern]'
 parser.add_option('-v', '--version', dest = 'version', action = 'store_true',
     help = "Display version and exit")
+parser.add_option('-b', '--back', dest = 'back', action = 'store_true',
+    help = "Rotate files that match the pattern back")
 opts, files = parser.parse_args()
 
 if opts.version:
