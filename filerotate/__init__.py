f18bd6ba7cda841967a52fbc554951b5b7c823a2
Alen Mujezinovic <flashingpumpkin@gmail.com>
2012-04-16 14:52:18 +0100

    Keep things DRY

--- filerotate/__init__.py
+++ filerotate/__init__.py
@@ -3,38 +3,40 @@ import __version__
 
 __version__ = __version__.__version__
 
-def forward(files):
-    files.sort()
-    files.reverse()
-
-    for file in files:
-        try:
-            name, ext = os.path.splitext(file)
-            os.rename(file, "{0}.{1}".format(name, int(ext[1:]) + 1))
-        except ValueError: # Not a number extension
-            os.rename(file, "{0}.{1}".format(file, 0))    
+def forward(file):
+    try:
+        name, ext = os.path.splitext(file)
+        os.rename(file, "{0}.{1}".format(name, int(ext[1:]) + 1))
+    except ValueError: # Not a number extension
+        os.rename(file, "{0}.{1}".format(file, 0)) 
             
-def backward(files):
-    files.sort()
+def backward(file):
+    try:
+        name, ext = os.path.splitext(file)
+        ext = int(ext[1:])
+        if ext == 0:
+            os.rename(file, name)
+        else:
+            os.rename(file, "{0}.{1}".format(name, ext - 1))
+    except ValueError: # Not a number extension, nothing to rotate
+        pass
 
+def iter(files, rename):
     for file in files:
-        try:
-            name, ext = os.path.splitext(file)
-            ext = int(ext[1:])
-            if ext == 0:
-                os.rename(file, name)
-            else:
-                os.rename(file, "{0}.{1}".format(name, ext - 1))
-        except ValueError: # Not a number extension, nothing to rotate
-            pass
+        if not os.path.isdir(file) and not os.path.isfile(file):
+            continue
+        rename(file)
 
 def rotate():
     from __main__ import opts, files
 
+    files.sort()
+    
     if opts.back:
-        backward(files)
+        iter(files, backward)
     else:
-        forward(files)
+        files.reverse()
+        iter(files, forward)
     
 
 if __name__ == "__main__":
